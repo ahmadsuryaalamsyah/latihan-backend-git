581f659c02d6c12e3732be286ec03942bffbf39f
Ahmad Surya Alam Syah <alamsyah200527@gmail.com>
2024-09-07 17:58:55 +0700

    Add product management endpoints

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,26 @@ app.use(express.json());
 
 // Endpoint untuk register user
 app.post('/register', (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    res.send('User registered');
+  const { username, password } = req.body;
+  res.send('User registered');
 });
 
 // Endpoint untuk login user
 app.post('/login', (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    res.send('User logged in');
+  const { username, password } = req.body;
+  res.send('User logged in');
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
+});
+
+app.post('/products', (req: Request, res: Response) => {
+  const { name, price } = req.body;
+  res.send('Product added');
+});
+
+// Endpoint untuk mendapatkan semua produk
+app.get('/products', (req: Request, res: Response) => {
+  res.send('List of products');
 });
